efb717e99a7ce17ce10dd8fed4a8c978252dcf85
Edward Beech <initialed85@gmail.com>
2026-09-16 22:32:46 +0800

    fix: remove FieldUpdate TokenizeTasks that injected template markers
    Remove TokenizeTasks from FieldUpdate parse task that were injecting nested
    Go template markers ({{ .FormatFunc }}, {{ .StructField }}) into KeepMatch.
    
    These markers caused template parse errors when KeepMatch was parsed again
    during main template generation. The case statements are now generated
    concretely in template.go without any tokenization step.

--- pkg/template/parse.go
+++ pkg/template/parse.go
@@ -700,33 +700,7 @@ func getParseTasks() []ParseTask {
 			StartExpr: regexp.MustCompile(`(?ms)^[ |\t]*// <field-update-methods>$\n`),
 			KeepExpr:  regexp.MustCompile(`(?ms).*`),
 			EndExpr:   regexp.MustCompile(`(?ms)^[ |\t]*// </field-update-methods>$\n`),
-			TokenizeTasks: []TokenizeTask{
-				{
-					Find:    regexp.MustCompile(`types\.IsZeroTime`),
-					Replace: "{{ .IsZeroFunc }}",
-				},
-				{
-					Find:    regexp.MustCompile(`types\.FormatTime`),
-					Replace: "{{ .FormatFunc }}",
-				},
-				{
-					Find:    regexp.MustCompile(`types\.FormatString`),
-					Replace: "{{ .FormatFunc }}",
-				},
-				{
-					Find:    regexp.MustCompile(`types\.FormatUUID`),
-					Replace: "{{ .FormatFunc }}",
-				},
-				{
-					Find:    regexp.MustCompile(`m\.CreatedAt`),
-					Replace: "m.{{ .StructField }}",
-				},
-				{
-					Find:    regexp.MustCompile(`TableNameTableCreatedAtColumn`),
-					Replace: "{{ .TableName }}Table{{ .StructField }}Column",
-				},
-			},
-			KeepIsPerColumn:            false,
+		TokenizeTasks: []TokenizeTask{},
 			KeepIsForPrimaryKeyOnly:    false,
 			KeepIsForNonPrimaryKeyOnly: false,
 			KeepIsForForeignKeysOnly:   false,
